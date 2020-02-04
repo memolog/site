9881d44fa9b222c0b375fac859a1e3e1979f562a
Yutaka Yamaguchi <yyamaguchi@sixapart.com>
2020-02-04 10:21:07 +0900

    Site updated: 2020-02-04 10:21:06

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,8 @@
-const CACHE_MAIN = 'main1556771887393';
+const CACHE_MAIN = 'main1580778984047';
 const precacheList = [
   
   '/',
+  '/2020/load-jquery-in-developer-console.html',
   '/2019/book-the-4th-revolution.html',
   '/2019/generate-anki-cards-tool.html',
   '/2018/merge-sort-in-javascript.html',
@@ -10,8 +11,8 @@ const precacheList = [
   '/2018/create-a-new-icon.html',
   '/2018/add-json-ld-structured-data.html',
   '/2018/bubble-sort-in-javascript.html',
-  '/2018/about-typedarray-prototype-sort.html',
   '/2018/a11y-fixes-for-prism-default-theme.html',
+  '/assets/images/harsh-jain-xoMeq3-GwTY-unsplash/harsh-jain-xoMeq3-GwTY-unsplash.svg',
   '/assets/images/sorry-imkirk-215248-unsplash/sorry-imkirk-215248-unsplash.svg',
   '/assets/images/kate-krivanec-212730-unsplash/kate-krivanec-212730-unsplash.svg',
   '/assets/images/tj-holowaychuk-62184-unsplash/tj-holowaychuk-62184-unsplash.svg',
@@ -20,7 +21,6 @@ const precacheList = [
   '/assets/images/new-icon/new-icon.svg',
   '/assets/images/thomas-kelley-75110-unsplash/thomas-kelley-75110-unsplash.svg',
   '/assets/images/jong-marshes-458354-unsplash/jong-marshes-458354-unsplash.svg',
-  '/assets/images/marcus-depaula-43304-unsplash/marcus-depaula-43304-unsplash.svg',
   '/assets/images/stas-ovsky-781959-unsplash/stas-ovsky-781959-unsplash.svg',
   '/css/prism.css',
   '/css/global_async.css',
