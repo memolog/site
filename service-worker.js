4721cc9b54c4b09d05cd5372acbdf29160c8d9c1
Yutaka Yamaguchi <yyamaguchi@sixapart.com>
2020-02-16 23:34:34 +0900

    Site updated: 2020-02-16 23:34:33

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,23 @@
-const CACHE_MAIN = 'main1581801413166';
+const CACHE_MAIN = 'main1581863641081';
 const precacheList = [
   
   '/',
+  '/2020/css-display-module-introduction.html',
   '/2020/about-position-sticky.html',
   '/2020/web-workers-lifetime.html',
   '/2020/convert-video-files-with-ffmpegjs-worker.html',
   '/2020/n2-sort-algorithms-again.html',
   '/2020/convert-screen-capture-with-ffmpeg.html',
-  '/2020/load-jquery-in-developer-console.html',
   '/2020/optional-chaining-and-nullish-coalescing.html',
   '/2020/web-workers-work-on-ie10.html',
   '/2020/top-level-await-will-be-soon.html',
   '/2020/event-target-and-currenttarget.html',
+  '/assets/images/saad-chaudhry-pM2Hpsi-Hxs-unsplash/saad-chaudhry-pM2Hpsi-Hxs-unsplash.svg',
   '/assets/images/david-travis-WC6MJ0kRzGw-unsplash/david-travis-WC6MJ0kRzGw-unsplash.svg',
   '/assets/images/harley-davidson-4ixHdlcROPI-unsplash/harley-davidson-4ixHdlcROPI-unsplash.svg',
   '/assets/images/greg-rakozy-0LU4vO5iFpM-unsplash/greg-rakozy-0LU4vO5iFpM-unsplash.svg',
   '/assets/images/neringa-sidlauskaite-YGWODwrWi4k-unsplash/neringa-sidlauskaite-YGWODwrWi4k-unsplash.svg',
   '/assets/images/jakob-owens-CiUR8zISX60-unsplash/jakob-owens-CiUR8zISX60-unsplash.svg',
-  '/assets/images/harsh-jain-xoMeq3-GwTY-unsplash/harsh-jain-xoMeq3-GwTY-unsplash.svg',
   '/assets/images/ben-hershey-1IZBAlIs4ug-unsplash/ben-hershey-1IZBAlIs4ug-unsplash.svg',
   '/assets/images/josue-isai-ramos-figueroa-Pj4je7OjrME-unsplash/josue-isai-ramos-figueroa-Pj4je7OjrME-unsplash.svg',
   '/assets/images/kai-pilger-1k3vsv7iIIc-unsplash/kai-pilger-1k3vsv7iIIc-unsplash.svg',
