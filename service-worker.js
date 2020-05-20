d0da8db4d4507c21e6680ea8b3e346a2dbaa50f4
Yutaka Yamaguchi <yyamaguchi@sixapart.com>
2020-05-21 00:02:56 +0900

    Site updated: 2020-05-21 00:02:55

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,8 @@
-const CACHE_MAIN = 'main1589795535294';
+const CACHE_MAIN = 'main1589986923885';
 const precacheList = [
   
   '/',
+  '/2020/flex-flow-direction.html',
   '/2020/automatic-minimum-size-of-flex-items.html',
   '/2020/flex-collpased-items.html',
   '/2020/flex-item-margin-padding-and-z-ordering.html',
@@ -11,7 +12,7 @@ const precacheList = [
   '/2020/flex-layout-box-model-and-terminology.html',
   '/2020/css-flexible-box-layout-module-introduction.html',
   '/2020/display-module-glossary.html',
-  '/2020/automatic-box-type-transformations.html',
+  '/assets/images/juliana-arruda-iVPWGCbFwd8-unsplash/juliana-arruda-iVPWGCbFwd8-unsplash.svg',
   '/assets/images/febiyan-z85gD0sTOQ0-unsplash/febiyan-z85gD0sTOQ0-unsplash.svg',
   '/assets/images/nong-vang-w5tI4WACAKo-unsplash/nong-vang-w5tI4WACAKo-unsplash.svg',
   '/assets/images/marc-klemm-rXDoLi1nqJo-unsplash/marc-klemm-rXDoLi1nqJo-unsplash.svg',
@@ -21,7 +22,6 @@ const precacheList = [
   '/assets/images/greg-jeanneau-ZUSyn7L6o68-unsplash/greg-jeanneau-ZUSyn7L6o68-unsplash.svg',
   '/assets/images/mick-haupt-XMLRK8xpEIs-unsplash/mick-haupt-XMLRK8xpEIs-unsplash.svg',
   '/assets/images/francesco-ungaro-JHypHcOObf4-unsplash/francesco-ungaro-JHypHcOObf4-unsplash.svg',
-  '/assets/images/jeremy-bishop-t52E8yzCURM-unsplash/jeremy-bishop-t52E8yzCURM-unsplash.svg',
   '/css/prism.css',
   '/css/global_async.css',
   '/js/lazysizes/lazysizes.min.js',
